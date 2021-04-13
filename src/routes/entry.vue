5e17f95e0fe4b36621a4d2eadae304d3c6fa4b06
鲁飞 <fei.lu0@geely.com>
2021-04-13 16:37:05 +0800

    update

--- src/routes/entry.vue
+++ src/routes/entry.vue
@@ -1,3 +1,16 @@
+<script lang="ts">
+  export default Vue.extend({
+    data() {
+      return {
+        name: 'okfd'
+      }
+    }
+  })
+</script>
+
 <template>
-  <p>hello, dude.</p>
+  <section>
+    <p v-text="name"></p>
+    <button class="btn" @click="name = 123">ok</button>
+  </section>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,115 @@
-const
-  os = require('os'),
-  path = require('path'),
-  webpack = require('webpack'),
-  {VueLoaderPlugin} = require('vue-loader'),
-  HtmlWebpackPlugin = require('html-webpack-plugin'),
-  MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const os = require('os')
+const path = require('path')
+const webpack = require('webpack')
+const {VueLoaderPlugin} = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const prod = process.argv.includes('-p')
 
-const conf = {
-  entry: [
-    'bootstrap/dist/css/bootstrap.min.css',
-    './src/app.js',
-    './src/style.less'
-  ],
+module.exports = ({env, publicPath}) => {
+  const prod = env === 'prod'
+  const conf = {
+    entry: [
+      'bootstrap/dist/css/bootstrap.min.css',
+      './src/app.js',
+      './src/style.less'
+    ],
 
-  output: {
-    path: path.resolve('dist'),
-    filename: `app.[${prod ? 'contenthash' : 'hash'}].js`
-  },
+    output: {
+      path: path.resolve('dist'),
+      publicPath: publicPath ?? '/',
+      filename: '[name].[contenthash:8].js',
+      chunkFilename: '[name].[contenthash:8].js',
+    },
 
-  resolve: {
-    alias: {
-      '@': path.resolve('.')
-    }
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve('.')
+      }
+    },
 
-  devServer: {
-    hot: true,
-    host: '0.0.0.0',
-  },
+    devServer: {
+      hot: true,
+      host: '0.0.0.0',
+      stats: 'errors-only',
+      contentBase: '.',
+      historyApiFallback: true,
+      overlay: {errors: true}
+    },
 
-  devtool: prod ? false : 'source-map',
+    devtool: prod ? false : 'source-map',
 
-  stats: 'errors-only',
+    stats: 'errors-only',
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          compilerOptions: {
-            preserveWhitespace: false
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            compilerOptions: {
+              preserveWhitespace: false
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          use: [prod ? MiniCssExtractPlugin.loader : 'vue-style-loader', 'css-loader', 'postcss-loader']
+        },
+        {
+          test: /\.less$/,
+          use: [prod ? MiniCssExtractPlugin.loader : 'vue-style-loader', 'css-loader', 'postcss-loader', 'less-loader']
+        },
+        {
+          test: /\.txt$/,
+          use: ['raw-loader']
+        },
+        {
+          test: /\.(jpg|png|jpeg|gif|svg)$/,
+          use: ['url-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: [prod ? MiniCssExtractPlugin.loader : 'vue-style-loader', 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.less$/,
-        use: [prod ? MiniCssExtractPlugin.loader : 'vue-style-loader', 'css-loader', 'postcss-loader', 'less-loader']
-      },
-      {
-        test: /\.txt$/,
-        use: ['raw-loader']
-      },
-      {
-        test: /\.(jpg|png|jpeg|gif|svg)$/,
-        use: ['url-loader']
+      ]
+    },
+
+    plugins: [
+      new VueLoaderPlugin(),
+
+      new webpack.ProvidePlugin({
+        PIXI: 'pixi.js',
+        Prism: 'prismjs',
+        Router: ['vue-router', 'default'],
+        Vue: ['vue/dist/vue.esm.js', 'default'],
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/layout.html',
+        filename: 'index.html',
+        inject: 'body',
+        minify: {
+          collapseWhitespace: true
+        }
+      })
+    ],
+
+    mode: prod ? 'production' : 'development'
+  }
+
+  if (prod) {
+    conf.plugins.push(new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    }))
+
+    conf.optimization = {
+      splitChunks: {
+        chunks: 'all'
       }
-    ]
-  },
-
-  plugins: [
-    new VueLoaderPlugin(),
-
-    new webpack.ProvidePlugin({
-      PIXI: 'pixi.js',
-      Prism: 'prismjs',
-      Router: ['vue-router', 'default'],
-      Vue: ['vue/dist/vue.esm.js', 'default'],
-    }),
-
-    new HtmlWebpackPlugin({
-      template: './src/layout.html',
-      filename: 'index.html',
-      inject: 'body',
-      minify: {
-        collapseWhitespace: true
-      }
-    })
-  ],
-
-  mode: prod ? 'production' : 'development'
-}
-
-if (prod) {
-  conf.plugins.push(new MiniCssExtractPlugin({
-    filename: 'style.[contenthash].css'
-  }))
-
-  conf.optimization = {
-    splitChunks: {
-      chunks: 'all'
     }
   }
-}
 
-module.exports = conf
+  return conf
+}
